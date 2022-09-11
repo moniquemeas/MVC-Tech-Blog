789b12470eef866ca9829f2639dbc065676b9ce7
Monique <monique.meas@gmail.com>
2022-09-11 13:21:52 -0400

    render hompage and login page using handlebars

---
+++ controller/home-routes.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: ['id', 'description', 'title', 'created_at'],
+
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+        
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({plain: true}));
+       
+        res.render('homepage', {posts});
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  router.get('/login', (req, res) => {
+    res.render('login');
+  });
+module.exports = router;
--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js')
 
 //route for api
 router.use('/api', apiRoutes);
-
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
